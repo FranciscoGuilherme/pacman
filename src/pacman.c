882e06203feee8dc4a76082febf6dfa7537b1e34
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-12 10:54:57 -0300

    feat(output): :sparkles: implementa a rotina de criacao do arquivo de estatisticas.

--- src/pacman.c
+++ src/pacman.c
@@ -134,16 +134,31 @@ void create_ranking_file(data *data)
     fclose(file);
 }
 
-//TODO: finalizar a logica de statisticas
 void create_statistics_file(data *data)
 {
     char *statistics_file = create_output_files_path(
         data->input.directory,
         STATISTICS_FILE_NAME
     );
+    int moviments_done =
+        data->output.w_statistics.moviments +
+        data->output.a_statistics.moviments +
+        data->output.s_statistics.moviments +
+        data->output.d_statistics.moviments;
+    short int moviments_wall_colision =
+        data->output.w_statistics.moviments_wall_colision +
+        data->output.a_statistics.moviments_wall_colision +
+        data->output.s_statistics.moviments_wall_colision +
+        data->output.d_statistics.moviments_wall_colision;
 
     FILE *file = fopen(statistics_file, "w");
-
+    fprinf(file, "Numero de movimentos: %d\n", moviments_done);
+    fprinf(file, "Numero de movimentos sem pontuar: %d\n", data->output.moviments_without_food);
+    fprinf(file, "Numero de colisoes com parede: %d\n", moviments_wall_colision);
+    fprinf(file, "Numero de movimentos para baixo: %d\n", data->output.s_statistics.moviments);
+    fprinf(file, "Numero de movimentos para cima: %d\n", data->output.w_statistics.moviments);
+    fprinf(file, "Numero de movimentos para esquerda: %d\n", data->output.a_statistics.moviments);
+    fprinf(file, "Numero de movimentos para direita: %d\n", data->output.d_statistics.moviments);
     fclose(file);
 }
 
