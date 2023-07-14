d52efbfb3f964efec9e6c078e97a0ea5e83368cb
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-14 13:37:34 -0300

    fix(structures): :bug: adiciona retorno para cenario nao mapeado.

--- src/structures/output.c
+++ src/structures/output.c
@@ -8,6 +8,7 @@ short int get_food_statistics(struct output_data *output, char letter)
         case 'a': return output->a_statistics.moviments_food_taken;
         case 's': return output->s_statistics.moviments_food_taken;
         case 'd': return output->d_statistics.moviments_food_taken;
+        default: return 0;
     }
 }
 
@@ -19,6 +20,7 @@ short int get_wall_statistics(struct output_data *output, char letter)
         case 'a': return output->a_statistics.moviments_wall_colision;
         case 's': return output->s_statistics.moviments_wall_colision;
         case 'd': return output->d_statistics.moviments_wall_colision;
+        default: return 0;
     }
 }
 
@@ -30,5 +32,6 @@ short int get_moviments_statistics(struct output_data *output, char letter)
         case 'a': return output->a_statistics.moviments;
         case 's': return output->s_statistics.moviments;
         case 'd': return output->d_statistics.moviments;
+        default: return 0;
     }
 }
