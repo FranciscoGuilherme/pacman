e340c3c3ace24ce87f80f400284d19f6f2438435
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-14 14:07:16 -0300

    fix(project): :bug: coloca a inicializacao do diretorio apos a validacao de sua existencia.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "headers/input.h"
 #include "headers/pacman.h"
 #include "headers/helpers.h"
 
-void initialize(data *data)
+void initialize(data *data, char **argv)
 {
     data->output.food = 0;
     data->input.total_food = 0;
     data->input.ghosts.amount = 0;
+    data->input.directory = argv[1];
     data->output.moviments_without_food = 0;
     data->output.w_statistics.moviments = 0;
     data->output.w_statistics.moviments_food_taken = 0;
@@ -35,13 +37,9 @@ int main(int argc, char *argv[])
     char *map_file;
     char current_moviment;
 
-    data.input.directory = argv[1];
-
-    (void) argc;
-    (void) argv;
-
-    initialize(&data);
     directory_is_defined(argc > 1);
+    initialize(&data, argv);
+    destroy_initial_parameters(argc, argv);
     prepare_map_file_path(&data.input.directory, &map_file);
     read_data(&map_file, &data);
     create_starter_file(&data.input);
