78f3f0af1e7f1d6e6eedf62db74f85822b5e3743
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-16 16:14:14 -0300

    refactor(output): :recycle: modulariza a funcao de criacao do arquivo de inicializacao.

---
+++ src/headers/reports/startup.h
@@ -0,0 +1,8 @@
+#ifndef _STARTUP_H
+#define _STARTUP_H
+
+#include "../pacman.h"
+
+void create_startup_file(input *input);
+
+#endif
---
+++ src/reports/startup.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../headers/pacman.h"
+#include "../headers/helpers.h"
+
+void create_startup_file(input *input)
+{
+    char *start_file = create_output_files_path(
+        input->directory,
+        FILE_NAME_START
+    );
+
+    FILE *file = fopen(start_file, "w");
+
+    for (short int row = 0; row < input->rows; row++)
+    {
+        fprintf(file, "%s\n", input->original[row]);
+    }
+
+    fprintf(file, "Pac-Man comecara o jogo na linha %hu e coluna %hu\n",
+        input->pacman.row,
+        input->pacman.column
+    );
+
+    fclose(file);
+    free(start_file);
+}
