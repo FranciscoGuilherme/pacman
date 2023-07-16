a8290f700a2976f47f3b2d91af0a233de8b274ea
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2023-07-16 19:24:23 -0300

    fix(ghosts): :construction: corrige as regras de movimentacao dos fantasmas.

--- src/ghost.c
+++ src/ghost.c
@@ -1,5 +1,6 @@
 #include "headers/ghost.h"
 #include "headers/pacman.h"
+#include "headers/helpers.h"
 #include "headers/reports/summary.h"
 #include "headers/validations/up.h"
 #include "headers/validations/down.h"
@@ -23,43 +24,84 @@ void move_ghosts(data *data, char moviment)
 
 void ghost_up_actions(data *data, ghost *ghost, char moviment)
 {
-    if (is_wall_up(data->input.original, &ghost->position)) ghost->direction = GHOST_DOWN;
+    if (is_wall_up(data->input.original, &ghost->position))
+    {
+        ghost->direction = GHOST_DOWN;
+
+        return;
+    }
+
     if (is_tunel_up(data->input.original, &ghost->position)) //TODO: implementar teleporte para ghost
-    if (is_player_up(data->input.original, &ghost->position)) {
+    if (is_player_up(data->input.original, &ghost->position))
+    {
         ghost->position.row -= 1;
+
         update_summary_file(&data->output, moviment, MESSAGE_GHOST_COLISION);
         game_over(data);
     }
+
+    ghost->position.row -= 1;
 }
 
 void ghost_down_actions(data *data, ghost *ghost, char moviment)
 {
-    if (is_wall_down(data->input.original, &ghost->position)) ghost->direction = GHOST_DOWN;
+    if (is_wall_down(data->input.original, &ghost->position))
+    {
+        ghost->direction = GHOST_DOWN;
+
+        return;
+    }
+
     if (is_tunel_down(data->input.original, &ghost->position)) //TODO: implementar teleporte para ghost
-    if (is_player_down(data->input.original, &ghost->position)) {
+    if (is_player_down(data->input.original, &ghost->position))
+    {
         ghost->position.row += 1;
+
+        update_summary_file(&data->output, moviment, MESSAGE_GHOST_COLISION);
         game_over(data);
     }
+
+    ghost->position.row += 1;
 }
 
 void ghost_left_actions(data *data, ghost *ghost, char moviment)
 {
-    if (is_wall_left(data->input.original, &ghost->position)) ghost->direction = GHOST_DOWN;
+    if (is_wall_left(data->input.original, &ghost->position))
+    {
+        ghost->direction = GHOST_DOWN;
+
+        return;
+    }
+
     if (is_tunel_left(data->input.original, &ghost->position)) //TODO: implementar teleporte para ghost
-    if (is_player_left(data->input.original, &ghost->position)) {
+    if (is_player_left(data->input.original, &ghost->position))
+    {
         ghost->position.column -= 1;
+
         update_summary_file(&data->output, moviment, MESSAGE_GHOST_COLISION);
         game_over(data);
     }
+
+    ghost->position.column -= 1;
 }
 
 void ghost_right_actions(data *data, ghost *ghost, char moviment)
 {
-    if (is_wall_right(data->input.original, &ghost->position)) ghost->direction = GHOST_DOWN;
+    if (is_wall_right(data->input.original, &ghost->position))
+    {
+        ghost->direction = GHOST_DOWN;
+
+        return;
+    }
+
     if (is_tunel_right(data->input.original, &ghost->position)) //TODO: implementar teleporte para ghost
-    if (is_player_right(data->input.original, &ghost->position)) {
+    if (is_player_right(data->input.original, &ghost->position))
+    {
         ghost->position.column += 1;
+
         update_summary_file(&data->output, moviment, MESSAGE_GHOST_COLISION);
         game_over(data);
     }
+
+    ghost->position.column += 1;
 }
